fd06a2ee917af7083bcaa111d1fd22bf77c69309
ruczak-m <ruczak-m@webmail.uwinnipeg>
2019-05-23 13:53:27 -0500

    Created a Class called Card which will be used to create a Card Object

---
+++ Flash Card/Flash Card/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flash_Card
+{
+    class Card
+    {
+        //Fields
+        private string question, answer; //These hold the question and the answer for this card
+
+        //Constructor
+        public Card(string question, string answer)
+        {
+            this.question = question;
+            this.answer = answer;
+        }
+
+        //Returns the question
+        public string getQuestion() { return question; }
+
+        //Returns the answer
+        public string getAnswer() { return answer; }
+    }
+}
