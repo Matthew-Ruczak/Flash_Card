ea9859d2e8354021e3f2026faa9838bfdf9a9afd
ruczak-m <ruczak-m@webmail.uwinnipeg>
2019-05-23 13:11:06 -0500

    Created Basic Layout for Using Flash Cards

--- Flash Card/Flash Card/CreateAndModify.Designer.cs
+++ Flash Card/Flash Card/CreateAndModify.Designer.cs
@@ -40,71 +40,77 @@
             // labelInfo1
             // 
             this.labelInfo1.AutoSize = true;
-            this.labelInfo1.Location = new System.Drawing.Point(12, 22);
+            this.labelInfo1.Location = new System.Drawing.Point(8, 14);
+            this.labelInfo1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelInfo1.Name = "labelInfo1";
-            this.labelInfo1.Size = new System.Drawing.Size(112, 20);
+            this.labelInfo1.Size = new System.Drawing.Size(74, 13);
             this.labelInfo1.TabIndex = 0;
             this.labelInfo1.Text = "Current Cards:";
             // 
             // listBoxOfQuestions
             // 
             this.listBoxOfQuestions.FormattingEnabled = true;
-            this.listBoxOfQuestions.ItemHeight = 20;
-            this.listBoxOfQuestions.Location = new System.Drawing.Point(16, 62);
+            this.listBoxOfQuestions.Location = new System.Drawing.Point(11, 40);
+            this.listBoxOfQuestions.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBoxOfQuestions.Name = "listBoxOfQuestions";
-            this.listBoxOfQuestions.Size = new System.Drawing.Size(567, 204);
+            this.listBoxOfQuestions.Size = new System.Drawing.Size(379, 134);
             this.listBoxOfQuestions.TabIndex = 1;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(107, 272);
+            this.btnAdd.Location = new System.Drawing.Point(71, 177);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(104, 63);
+            this.btnAdd.Size = new System.Drawing.Size(69, 41);
             this.btnAdd.TabIndex = 2;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
             // 
             // btnModify
             // 
-            this.btnModify.Location = new System.Drawing.Point(239, 272);
+            this.btnModify.Location = new System.Drawing.Point(159, 177);
+            this.btnModify.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnModify.Name = "btnModify";
-            this.btnModify.Size = new System.Drawing.Size(103, 63);
+            this.btnModify.Size = new System.Drawing.Size(69, 41);
             this.btnModify.TabIndex = 3;
             this.btnModify.Text = "Modify";
             this.btnModify.UseVisualStyleBackColor = true;
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(368, 272);
+            this.btnDelete.Location = new System.Drawing.Point(245, 177);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(111, 63);
+            this.btnDelete.Size = new System.Drawing.Size(74, 41);
             this.btnDelete.TabIndex = 4;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(12, 385);
+            this.btnExit.Location = new System.Drawing.Point(8, 241);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(107, 28);
+            this.btnExit.Size = new System.Drawing.Size(71, 27);
             this.btnExit.TabIndex = 5;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(476, 385);
+            this.btnSave.Location = new System.Drawing.Point(317, 241);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(107, 28);
+            this.btnSave.Size = new System.Drawing.Size(71, 27);
             this.btnSave.TabIndex = 6;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
             // 
             // CreateAndModify
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(595, 425);
+            this.ClientSize = new System.Drawing.Size(397, 276);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnDelete);
@@ -113,6 +119,7 @@
             this.Controls.Add(this.listBoxOfQuestions);
             this.Controls.Add(this.labelInfo1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "CreateAndModify";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
---
+++ Flash Card/Flash Card/UsingFlashCards.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Flash_Card
+{
+    partial class UsingFlashCards
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxQuestion = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxAnswer = new System.Windows.Forms.RichTextBox();
+            this.btnShowAnswer = new System.Windows.Forms.Button();
+            this.btnNextQuestion = new System.Windows.Forms.Button();
+            this.btnPreviousQuestion = new System.Windows.Forms.Button();
+            this.labelInfo1 = new System.Windows.Forms.Label();
+            this.labelInfo2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBoxQuestion
+            // 
+            this.richTextBoxQuestion.Location = new System.Drawing.Point(73, 53);
+            this.richTextBoxQuestion.Name = "richTextBoxQuestion";
+            this.richTextBoxQuestion.Size = new System.Drawing.Size(340, 78);
+            this.richTextBoxQuestion.TabIndex = 0;
+            this.richTextBoxQuestion.Text = "";
+            // 
+            // richTextBoxAnswer
+            // 
+            this.richTextBoxAnswer.Location = new System.Drawing.Point(73, 179);
+            this.richTextBoxAnswer.Name = "richTextBoxAnswer";
+            this.richTextBoxAnswer.Size = new System.Drawing.Size(340, 78);
+            this.richTextBoxAnswer.TabIndex = 1;
+            this.richTextBoxAnswer.Text = "";
+            // 
+            // btnShowAnswer
+            // 
+            this.btnShowAnswer.Location = new System.Drawing.Point(177, 276);
+            this.btnShowAnswer.Name = "btnShowAnswer";
+            this.btnShowAnswer.Size = new System.Drawing.Size(135, 23);
+            this.btnShowAnswer.TabIndex = 2;
+            this.btnShowAnswer.Text = "Show Answer";
+            this.btnShowAnswer.UseVisualStyleBackColor = true;
+            // 
+            // btnNextQuestion
+            // 
+            this.btnNextQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNextQuestion.Location = new System.Drawing.Point(419, 63);
+            this.btnNextQuestion.Name = "btnNextQuestion";
+            this.btnNextQuestion.Size = new System.Drawing.Size(57, 184);
+            this.btnNextQuestion.TabIndex = 3;
+            this.btnNextQuestion.Text = ">";
+            this.btnNextQuestion.UseVisualStyleBackColor = true;
+            // 
+            // btnPreviousQuestion
+            // 
+            this.btnPreviousQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPreviousQuestion.Location = new System.Drawing.Point(10, 63);
+            this.btnPreviousQuestion.Name = "btnPreviousQuestion";
+            this.btnPreviousQuestion.Size = new System.Drawing.Size(57, 184);
+            this.btnPreviousQuestion.TabIndex = 4;
+            this.btnPreviousQuestion.Text = "<";
+            this.btnPreviousQuestion.UseVisualStyleBackColor = true;
+            // 
+            // labelInfo1
+            // 
+            this.labelInfo1.AutoSize = true;
+            this.labelInfo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInfo1.Location = new System.Drawing.Point(189, 26);
+            this.labelInfo1.Name = "labelInfo1";
+            this.labelInfo1.Size = new System.Drawing.Size(94, 24);
+            this.labelInfo1.TabIndex = 5;
+            this.labelInfo1.Text = "Question";
+            // 
+            // labelInfo2
+            // 
+            this.labelInfo2.AutoSize = true;
+            this.labelInfo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInfo2.Location = new System.Drawing.Point(203, 152);
+            this.labelInfo2.Name = "labelInfo2";
+            this.labelInfo2.Size = new System.Drawing.Size(80, 24);
+            this.labelInfo2.TabIndex = 6;
+            this.labelInfo2.Text = "Answer";
+            // 
+            // UsingFlashCards
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 311);
+            this.Controls.Add(this.labelInfo2);
+            this.Controls.Add(this.labelInfo1);
+            this.Controls.Add(this.btnPreviousQuestion);
+            this.Controls.Add(this.btnNextQuestion);
+            this.Controls.Add(this.btnShowAnswer);
+            this.Controls.Add(this.richTextBoxAnswer);
+            this.Controls.Add(this.richTextBoxQuestion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "UsingFlashCards";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flash Card: nameOfFlashCard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxQuestion;
+        private System.Windows.Forms.RichTextBox richTextBoxAnswer;
+        private System.Windows.Forms.Button btnShowAnswer;
+        private System.Windows.Forms.Button btnNextQuestion;
+        private System.Windows.Forms.Button btnPreviousQuestion;
+        private System.Windows.Forms.Label labelInfo1;
+        private System.Windows.Forms.Label labelInfo2;
+    }
+}
